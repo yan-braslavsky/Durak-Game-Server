71ab782ae8a752283f6717fb258898e04a9b7bb1
Yan Braslavskiy <iveliusster@gmail.com>
2015-05-27 20:14:53 +0200

    Initial commit

---
+++ src/main/java/com/yan/durak/server/lobby/Main.java
@@ -0,0 +1,28 @@
+package com.yan.durak.server.lobby;
+
+import com.yan.durak.server.lobby.servlets.LobbyServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * Created by Yan-Home on 5/27/2015.
+ */
+public class Main {
+
+    public static final String DEBUG_PORT = "1314";
+
+    public static void main(String[] args) throws Exception {
+        String port = System.getenv("PORT");
+        if(port == null)
+            port = DEBUG_PORT;
+
+        Server server = new Server(Integer.valueOf(port));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new LobbyServlet()), "/*");
+        server.start();
+        server.join();
+    }
+}
---
+++ src/main/java/com/yan/durak/server/lobby/servlets/LobbyServlet.java
@@ -0,0 +1,19 @@
+package com.yan.durak.server.lobby.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Yan-Home on 5/27/2015.
+ */
+public class LobbyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().print("Hello from Java built with Gradle!\n");
+    }
+}
